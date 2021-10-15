b1622866d030f1137a620c6e238bbeb9913e5579
Joost Zöllner <joost.zoellner@holisticon.de>
2021-10-15 15:05:34 +0200

    chore: update to Angular v11

--- projects/rest/src/lib/rest.module.spec.ts
+++ projects/rest/src/lib/rest.module.spec.ts
@@ -1,9 +1,9 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RestModule } from './rest.module';
 
 describe('RestModule', () => {
     beforeEach(
-        async(() => {
+        waitForAsync(() => {
             TestBed.configureTestingModule({
                 imports: [RestModule]
             }).compileComponents();
